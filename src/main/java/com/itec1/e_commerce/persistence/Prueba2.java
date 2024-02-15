4efd6316ba81fea430d71b14b8d3a7785843cbc4
JuancaSterba <sjcexe@gmail.com>
2024-02-15 20:15:14 -0300

    push de prueba

---
+++ src/main/java/com/itec1/e_commerce/persistence/Prueba2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itec1.e_commerce.persistence;
+
+/**
+ *
+ * @author sjcex
+ */
+public class Prueba2 {
+    
+}
