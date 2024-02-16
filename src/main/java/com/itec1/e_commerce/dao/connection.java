71d02b220fde54d398ce865fc2664dee6e6107ba
JuancaSterba <sjcexe@gmail.com>
2024-02-15 21:22:26 -0300

    se cambia ubicacion de paquete dao

--- src/main/java/com/itec1/e_commerce/dao/connection.java
+++ src/main/java/com/itec1/e_commerce/dao/connection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dao;
+package com.itec1.e_commerce.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
