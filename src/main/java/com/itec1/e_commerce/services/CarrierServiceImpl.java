b4d990d3132bddfadd6fff5b45b79c3f228aae7d
PaniaguaSanty <paniaguasanty10@gmail.com>
2024-03-04 11:56:56 -0300

    SearchByTypeOfTransportist modificada.

--- src/main/java/com/itec1/e_commerce/services/CarrierServiceImpl.java
+++ src/main/java/com/itec1/e_commerce/services/CarrierServiceImpl.java
@@ -18,8 +18,13 @@ public class CarrierServiceImpl implements ICRUD<Carrier> {
 
     @Override
     public Carrier create(Carrier entity) {
-        carrierJpaController.create(entity);
-        return carrierJpaController.findCarrier(entity.getId());
+        try {
+            carrierJpaController.create(entity);
+            return carrierJpaController.findCarrier(entity.getId());
+        } catch (Exception e) {
+            System.err.println("Error when creating the carrier: " + e.getMessage());
+            throw new RuntimeException("Failed to create carrier.", e);
+        }
     }
 
     @Override
@@ -80,17 +85,22 @@ public class CarrierServiceImpl implements ICRUD<Carrier> {
     }
 
     public List<Carrier> searchByTypeOfTransport(String transportType) {
-        return carrierJpaController.findCarrierEntities().stream()
-                .filter(carrier -> {
-                    if (transportType.equalsIgnoreCase("aerial")) {
-                        return carrier.getAerial() == true;
-                    } else if (transportType.equalsIgnoreCase("ground")) {
-                        return carrier.getGround() == true;
-                    } else if (transportType.equalsIgnoreCase("maritime")) {
-                        return carrier.getMaritime() == true;
-                    }
-                    return false;
-                })
-                .collect(Collectors.toList());
+        try {
+            return carrierJpaController.findCarrierEntities().stream()
+                    .filter(carrier -> {
+                        if (transportType.equalsIgnoreCase("aerial")) {
+                            return carrier.getAerial() == true;
+                        } else if (transportType.equalsIgnoreCase("ground")) {
+                            return carrier.getGround() == true;
+                        } else if (transportType.equalsIgnoreCase("maritime")) {
+                            return carrier.getMaritime() == true;
+                        }
+                        return false;
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.err.println("Error while searching for carriers by type of transport: " + e.getMessage());
+            throw new RuntimeException("Error while searching for carriers by type of transport. Please try again later.", e);
+        }
     }
 }
